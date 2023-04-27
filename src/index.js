cd08db9460d2e06a4a6eb509e4c7f629f29d89fd
JonaszChudobski <jonasz.chudobski@wp.pl>
2023-04-27 16:21:54 +0200

    styling of the app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { Alert } from "./components/Alert";
 
-const element = <h1 className="hello">Hello World!</h1>;
+const App = () => {
+  return (
+    <>
+      <Alert variant="info" outlined>
+        Would you like to?
+      </Alert>
+      <Alert variant="error" elevated>
+        There was some error during transaction
+      </Alert>
+      <Alert variant="success" outlined>
+        Payment received.
+      </Alert>
+      <Alert variant="warning" elevated>
+        Please update.
+      </Alert>
+    </>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(element);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
