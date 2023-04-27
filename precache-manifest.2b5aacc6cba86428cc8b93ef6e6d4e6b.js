760b842d71c0e1fcef2f2dddfe488bb5cc3b23cb
JonaszChudobski <jonasz.chudobski@wp.pl>
2023-04-27 16:21:20 +0200

    Updates

---
+++ precache-manifest.2b5aacc6cba86428cc8b93ef6e6d4e6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4da780ca61e9dbf992c",
+    "url": "/react-first-app/static/css/main.5065ce9a.chunk.css"
+  },
+  {
+    "revision": "f4da780ca61e9dbf992c",
+    "url": "/react-first-app/static/js/main.f4da780c.chunk.js"
+  },
+  {
+    "revision": "253a9b92d86cad704d92",
+    "url": "/react-first-app/static/js/1.253a9b92.chunk.js"
+  },
+  {
+    "revision": "32801449720e730e2668",
+    "url": "/react-first-app/static/js/runtime~main.32801449.js"
+  },
+  {
+    "revision": "18d9070830601ba2d255c02b445851f6",
+    "url": "/react-first-app/index.html"
+  }
+];
